98e630e84384934ef6319c321c3145444882a2f3
Maciej <maciej.b.lukasik@gmail.com>
2020-04-10 22:52:56 +0200

    DatfAcademy homework 1

---
+++ daft-1/src/index.js
@@ -0,0 +1,4 @@
+import './styles/style.sass';
+import fun from './script'
+
+fun()
---
+++ daft-1/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.sass$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.png$/,
+                exclude: /node_modules/,
+                use: ['file-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin(
+            {
+                template: './src/index.html',
+                filename: 'index.html'
+            }
+        )
+    ]
+}
